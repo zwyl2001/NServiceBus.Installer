9889b79c933180ce6a585e378e9b5576c5ce6f02
sfarmar <s.farmar@gmail.com>
2013-10-10 23:30:37 +0100

    Updated AIP and added script to populate binaries

--- src/NServiceBus/Support/unpackDependencies.ps1
+++ src/NServiceBus/Support/unpackDependencies.ps1
@@ -1,5 +1,7 @@
 [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
 
+
+
 $samplesZipFile = Resolve-Path '..\Dependencies'
 
 $targetSamplesFolder = Resolve-Path '..\..\samples'
@@ -11,11 +13,3 @@ $targetSamplesFolder = Resolve-Path '..\..\samples'
 [System.IO.Compression.ZipFile]::ExtractToDirectory("$samplesZipFile\NServiceBus.Samples.zip", $targetSamplesFolder)
 
 $copyToPackagesFolder = Resolve-Path '..\Dependencies\packages\*'
-
-$packagesFolder = Resolve-Path '..\..\packages\'
-
-
-Copy-Item $copyToPackagesFolder -Destination $packagesFolder -Recurse -force
-
-
-
