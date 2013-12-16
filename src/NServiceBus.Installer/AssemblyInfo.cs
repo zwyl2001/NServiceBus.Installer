daf518873fa112b038a919609f60efcbdbfdda5a
Simon Cropp <simon.cropp@gmail.com>
2013-12-16 18:57:36 +1100

    bump GFV

---
+++ src/NServiceBus.Installer/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("NServiceBus.Installer")]
+[assembly: AssemblyProduct("NServiceBus.Installer")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
+
